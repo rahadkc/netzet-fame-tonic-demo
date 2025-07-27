d5fb7368e8804ca6c97755c52e50ba0923819729
rahadkc <rahadkc@gmail.com>
2025-07-27 12:12:55 +0600

    Fonts: Add font utils and set figtree as default and urbanist as selective font

---
+++ src/utils/fonts.ts
@@ -0,0 +1,15 @@
+import { Urbanist, Figtree } from 'next/font/google';
+
+export const figtree = Figtree({
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-figtree',
+});
+
+export const urbanist = Urbanist({
+  weight: '700',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-urbanist',
+});
